f0d854dfd337a4919d15650d4b697d140fcb19c7
Ferid547 <igidov.ferid1@gmail.com>
2023-07-28 01:53:25 +0400

    First JavaScript example

---
+++ index.js
@@ -0,0 +1,54 @@
+//? Change places 7 5 //
+{
+let a = 5;
+let b = 7;
+let z = a = 7;
+let y = b = 5;
+
+console.log(a, b);
+}
+
+
+
+//? Perimeter and area of a square //
+{
+let a = 5;
+
+// //! The perimeter of the square //
+let perimetr = a * 4;
+console.log(perimetr);
+
+//* Area of the square //
+let area = a * a;
+console.log(area);
+}
+
+
+
+//? Perimeter and area of a rectangle //
+{
+let a = 10;
+let b = 15;
+
+//! The perimeter of the rectangle //
+
+let perimetr = (a + b)*2;
+console.log(perimetr);
+
+//* Area of a rectangle //
+
+let area = a * b;
+console.log(area);
+}
+
+
+
+//* username, password //
+let username = "Farid";
+let password = "12345678"
+
+username === "Farid" ? console.log("True!") : console.log("False!");
+password === "12345678" ? console.log("True!") : console.log("False!");
+
+
+
